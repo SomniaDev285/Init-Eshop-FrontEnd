9bd56c63de2319f7190c4f506bc02501e4d00e78
SomniaDev285 <paper.crane0121@gmail.com>
2024-03-24 14:54:26 +0330

    localization implemented in project #3

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './i18n'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Suspense fallback='...loading'>
+      <App />
+    </Suspense>
   </React.StrictMode>
 );
 
