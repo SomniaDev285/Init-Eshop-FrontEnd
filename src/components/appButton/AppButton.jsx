9858097bd7390f7d3a1829f00e2c7b59c278d33b
SomniaDev285 <paper.crane0121@gmail.com>
2024-03-26 12:27:28 +0330

    minor update

--- src/components/appButton/AppButton.jsx
+++ src/components/appButton/AppButton.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const AppButton = ({type, label}) => {
+const AppButton = ({ type, label }) => {
+  const [intensity, setIntensity] = useState(500)
   return (
     <div className="w-full h-full">
-      <button type={type ? type : ''} className="bg-blue-500 hover:bg-blue-700 text-white w-full h-full font-bold py-2 px-4 rounded-full">
+      <button type={type ? type : ''} className={[`bg-blue-${intensity}`, "hover:bg-blue-700 text-white w-full h-full font-bold py-2 px-4 rounded-full"]}>
         {label ? label : 'Button'}
       </button>
     </div>
