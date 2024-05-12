0f443ff66ac93ebed68853e1a12f079e6d78220e
SomniaDev285 <paper.crane0121@gmail.com>
2024-05-12 10:35:15 +0330

    minor update in routes

--- src/router/Router.js
+++ src/router/Router.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Welcome from '../views/welcome/Welcome'
 import Warehouse from '../views/warehouse/Warehouse'
 import Dashboard from '../views/dashboard/Dashboard'
+import Products from '../views/products/Products'
 
 const Router = () => {
     return (
@@ -11,6 +12,7 @@ const Router = () => {
                 <Route path="/" exact element={<Welcome />} />
                 <Route path='/dashboard' exact element={<Dashboard />}>
                     <Route index element={<p>Welcome to the Admin Panel Dashboard</p>} />
+                    <Route path='products' element={<Products />} />
                     <Route path="warehouse" element={<Warehouse />}>
                         <Route path=':warehouseId' element={<Warehouse />} />
                     </Route>
